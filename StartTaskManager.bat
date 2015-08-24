ad431f03db64336817194e689a21d09f35930801
jtrent238 <jtrent238@outlook.com>
2015-08-24 18:29:33 -0400

    Create StartTaskManager.bat

---
+++ StartTaskManager.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+color 5b
+echo                     **********************************
+echo                     TaskMananger Starter By:jtrent238
+echo                         youtube.com/thejtrent238
+echo                     **********************************
+echo 
+echo Starting...
+cls
+echo Task Mananger Started
+cls
+
+echo                       **********************************
+echo                              TaskMananger Starter
+echo                                 By:jtrent238
+echo                           youtube.com/thejtrent238
+echo                       **********************************
+echo                             ~~~Thanks For Using!~~~
+echo                        ----You May Close This Window----
+"C:\Windows\System32\taskmgr.exe"
+close
+exit
